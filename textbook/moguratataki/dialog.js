3392df4c0d5ec08b6ac151e2ff56d12d2ab98e4d
miyaguni-rikuto <120348141+miyaguni-rikuto@users.noreply.github.com>
2024-08-15 14:42:27 +0900

    もぐらたたき

---
+++ textbook/moguratataki/dialog.js
@@ -0,0 +1,14 @@
+var tentokun;
+
+
+window.onload=function(){
+    start();
+};
+
+
+function start(){
+    tetokun=document.getElementById("gazo");
+    tentokun.onclick=function(){
+        alert("たたかれた！");
+    };
+}
